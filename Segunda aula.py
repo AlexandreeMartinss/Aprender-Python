592018422df8b85b944d183a5183d21ffc74bea6
AlexandreeMartinss <142326631+AlexandreeMartinss@users.noreply.github.com>
2023-11-25 18:27:30 +0000

    Add files via upload

---
+++ Segunda aula.py
@@ -0,0 +1,93 @@
+"""
+Integers - Valores inteiros
+Floats - Valores reais
+Strings - Caracteres
+Boolean - Verdadeiro / Falso
+Para escrever comentarios carinal para linha de texto ou 3 aspas para bloco de texto
+"""
+"""
+i = 10
+j = 10.2
+nome = "Alexandre"
+verdadeiro = True
+
+print (i)
+print (j)
+print (nome)
+print (verdadeiro)
+
+
+pato = 3
+pato = 2
+
+print (pato)
+print (pato)
+
+
+
+#converter valores para inteiro
+floatparainteiro = int (2.7)
+stringparainteiro = int ("2")
+
+
+#converter valores para float
+intparafloat = float (1)
+stringparafloat = float ("2.5")
+
+#converter valores para string
+inteiroparastring = str (45)
+floatparastring = str (2.5)
+
+print(stringparainteiro+2)
+
+# Operadores
+x = 2 + 2
+x = 2 - 2
+x = 2 / 2
+x = 2 * 2
+
+x = 2 % 2
+x = 2 ** 2
+
+
+#Strings
+     #01234
+a = "Alexandre, Martins"
+print (a[1])
+
+print(a[2:5])
+
+print(len(a))
+
+print (a.lower())
+
+print(a.upper())
+
+print(a.replace("a","x"))
+
+b = a.split(",")
+print(b[0])
+print(b[1])
+
+#condiçoes
+
+a = 30
+b = 20
+if a < b :
+    print("a é menor do que b")
+elif a == b:
+    print("a é igual a b")
+else:
+     print("b é menor do que a")
+    
+    
+
+
+
+== comparar se sao iguais
+!= comparara se sao diferentes
+< comparar se o primeiro é menor do que o segundo
+> comparar de o primeiro é maior do que o segundo
+<= comparar se o primeiro é menor ou igual ao segundo
+>= comparar se o primeiro é maior ou igual ao segundo
+"""
